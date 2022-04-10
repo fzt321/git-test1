bb30d57582d0d2dbd3be1dc7b828cd56dc9b8230
“fzt321” <1751524287@qq.com>
2022-04-10 16:31:23 +0800

    第二次提交

--- src/main/java/com/atguigu/hit/GitTest1.java
+++ src/main/java/com/atguigu/hit/GitTest1.java
@@ -3,5 +3,6 @@ package com.atguigu.hit;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello idea+git");
+        System.out.println("牛逼的啊");
     }
 }
