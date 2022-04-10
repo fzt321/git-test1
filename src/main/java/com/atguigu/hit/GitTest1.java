8c7ec8ab72175e6ea650fd01358b023575647dc6
“fzt321” <1751524287@qq.com>
2022-04-10 17:07:22 +0800

    第hotfix五次提交

--- src/main/java/com/atguigu/hit/GitTest1.java
+++ src/main/java/com/atguigu/hit/GitTest1.java
@@ -3,7 +3,7 @@ package com.atguigu.hit;
 public class GitTest1 {
     public static void main(String[] args) {
         System.out.println("hello idea+git");
-        System.out.println("牛逼的啊");
+        System.out.println("我也不接受！！！");
         System.out.println("牛逼的啊");
         System.out.println("牛逼的啊");
         System.out.println("我不接受");
